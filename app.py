1d83bd024730f54cf5fd8fda4e647173946eda93
Ouwe-Douwe <ewoudnoe.business@gmail.com>
2023-10-22 10:36:31 +0200

    added some UI and database

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+def __repr__(self):
+    return '<Person %r>' % self.id
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/update/', methods=['GET', 'POST'])
+def addPerson():
+    return render_template('addperson.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
